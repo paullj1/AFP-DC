7d72e4c723611379cb4d418ef33f29baef1e0607
paullj1 <paullj1@gmail.com>
2015-12-14 14:07:25 -0500

    Switching to 'scikit-learn'

--- predictor.py
+++ predictor.py
@@ -1,20 +1,14 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
-import sys
-from svmutil import *
+from sklearn import svm
 
-print(sys.version)
-y, x = [1,-1], [{1:1, 3:1}, {1:-1,3:-1}]
-prob  = svm_problem(y, x)
-param = svm_parameter('-t 0 -c 4 -b 1')
-m = svm_train(prob, param)
+n_samples = [0,0]
+n_features = [1,1]
+training_samples = [n_samples, n_features]
+labels = [0,1]
 
-p_label, p_acc, p_val = svm_predict(y[1:], x[1:], m)
+clf = svm.SVC()
+clf.fit(training_samples,labels)
 
-print('')
-print('Label: ')
-print(p_label)
-print('Acc: ')
-print(p_acc)
-print('Val: ')
-print(p_val)
+print(clf.predict([[2.,2.]]))
+print(clf.predict([[0.,0.]]))
