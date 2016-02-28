464feca0fd661c94c1ed4275f79bacba4f6eb64d
Paul <paullj1@gmail.com>
2016-02-27 21:44:35 -0500

    Checking in

--- Load/RunLoadSim.ps1
+++ Load/RunLoadSim.ps1
@@ -3,12 +3,14 @@
 # Script assumes 'LocalLoadSimulator.ps1' and 'Connect-Mstsc.ps1' are on the desktop
 
 # If doing SMB share traffic generation, need to enable CredSSP as follows:
-# - On Controller: EnableWSManCredSSP -Role Client -DelegateComputer *.domain.com
-# - On Clients:    EnableWSManCredSSP -Role Server
+# - On Controller: Enable-WSManCredSSP -Role Client -DelegateComputer *.domain.com
+# - On Clients:    Enable-WSManCredSSP -Role Server
+#                  Enable-PSRemoting
 
+$net_use_name = 'Administrator'
 $net_use_pass = '!@12QWqwe'
-$client = 'client1.afnet.com'
-$time = 10
+$clients = "client1.afnet.com,client2.afnet.com,client3.afnet.com,client4.afnet.com,client5.afnet.com".Split(',')
+$time = 300
 $reqs_per_sec = 2
 $rdp_host = 'ts.afnet.com'
 $rdp_user = 'user1'
@@ -16,9 +18,11 @@ $rdp_pass = '!@12QWqwe'
 $net_root = '\\TS\Shared'
 $web_urls = 'espn.com,cnn.com,facebook.com,yahoo.com,news.google.com'
 
-$pw = $net_use_pass|ConvertTo-SecureString -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential($net_use_name,$pw)
-$s = New-PSSession -ComputerName $client -Credential $cred -Authentication Credssp
-Invoke-Command -Session $s -ScriptBlock { Set-Location ..\Desktop }
-Invoke-Command -Session $s -ScriptBlock { param($1, $2, $3, $4, $5, $6, $7) .\LocalLoadSimulator.ps1 $1 $2 $3 $4 $5 $6 $7 } -ArgumentList $time, $reqs_per_sec, $rdp_host, $rdp_user, $rdp_pass, $net_root, $web_urls
-Remove-PSSession $s
+foreach ( $client in $clients ) {
+    $pw = $net_use_pass|ConvertTo-SecureString -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential($net_use_name,$pw)
+    $s = New-PSSession -ComputerName $client -Credential $cred -Authentication Credssp
+    Invoke-Command -Session $s -ScriptBlock { Set-Location ..\Desktop }
+    Invoke-Command -Session $s -AsJob -ScriptBlock { param($1, $2, $3, $4, $5, $6, $7) .\LocalLoadSimulator.ps1 $1 $2 $3 $4 $5 $6 $7 } -ArgumentList $time, $reqs_per_sec, $rdp_host, $rdp_user, $rdp_pass, $net_root, $web_urls
+    
+}
